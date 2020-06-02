3e0653d03790c8967b0bb8dc567c14a3a139a194
Mark Pilgrim <mpilgrim@brave.com>
2020-06-02 13:42:20 -0400

    Fall through to upstream logic that allows some videos to autoplay muted
    use anonymous namesapce
    
    activation policy [fixes Jitsi autoplaying audio elements]

--- chromium_src/third_party/blink/renderer/core/html/media/autoplay_policy.cc
+++ chromium_src/third_party/blink/renderer/core/html/media/autoplay_policy.cc
@@ -4,22 +4,42 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "third_party/blink/public/platform/web_content_settings_client.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/html/media/html_media_element.h"
 
-#define BRAVE_AUTOPLAY_POLICY_IS_GESTURE_NEEDED_FOR_PLAYBACK \
-  if (IsAutoplayAllowedPerSettings())                        \
+namespace {
+
+bool IsAutoplayAllowedForFrame(blink::LocalFrame* frame) {
+  if (!frame)
+    return false;
+  if (auto* settings_client = frame->GetContentSettingsClient())
+    return settings_client->AllowAutoplay(true /* default_value */);
+  return true;
+}
+
+bool IsAutoplayAllowedForElement(
+    blink::Member<blink::HTMLMediaElement> element) {
+  return IsAutoplayAllowedForFrame(element->GetDocument().GetFrame());
+}
+
+}  // namespace
+
+#define BRAVE_AUTOPLAY_POLICY_GET_FOR_DOCUMENT        \
+  if (IsAutoplayAllowedForFrame(document.GetFrame())) \
+    return Type::kNoUserGestureRequired;              \
+  else                                                \
+    return Type::kUserGestureRequired;
+
+#define BRAVE_AUTOPLAY_POLICY_REQUEST_AUTOPLAY_BY_ATTRIBUTE \
+  if (!IsAutoplayAllowedForElement(element_))               \
     return false;
 
-#define BRAVE_AUTOPLAY_POLICY_IS_AUTOPLAY_ALLOWED_PER_SETTINGS         \
-  bool AutoplayPolicy::IsAutoplayAllowedPerSettings() const {          \
-    LocalFrame* frame = element_->GetDocument().GetFrame();            \
-    if (!frame)                                                        \
-      return false;                                                    \
-    if (auto* settings_client = frame->GetContentSettingsClient())     \
-      return settings_client->AllowAutoplay(true /* default_value */); \
-    return true;                                                       \
-  }
+#define BRAVE_AUTOPLAY_POLICY_REQUEST_PLAY    \
+  if (!IsAutoplayAllowedForElement(element_)) \
+    return DOMExceptionCode::kNotAllowedError;
 
 #include "../../../../../third_party/blink/renderer/core/html/media/autoplay_policy.cc"  // NOLINT
 
-#undef BRAVE_AUTOPLAY_POLICY_IS_GESTURE_NEEDED_FOR_PLAYBACK
-#undef BRAVE_AUTOPLAY_POLICY_IS_AUTOPLAY_ALLOWED_PER_SETTINGS
+#undef BRAVE_AUTOPLAY_POLICY_GET_FOR_DOCUMENT
+#undef BRAVE_AUTOPLAY_POLICY_REQUEST_AUTOPLAY_BY_ATTRIBUTE
+#undef BRAVE_AUTOPLAY_POLICY_REQUEST_PLAY
